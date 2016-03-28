535e204a122c13a1d41e0b2dc8a79926bcb8e395
tktr <no.82.vuf.lab@gmail.com>
2016-03-28 16:28:49 +0900

    hello world

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
